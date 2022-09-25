8873cbbaa9d200bf7e8596eab918a872a205c51c
Piero Anesin <piero.anesin@cherrychain.it>
2022-09-25 13:23:12 +0200

    refactored flower packaging for lilies

--- src/main/kotlin/it/anesin/DefaultPackaging.kt
+++ src/main/kotlin/it/anesin/DefaultPackaging.kt
@@ -17,29 +17,24 @@ class DefaultPackaging: Packaging {
     return result.filter { it.first > 0 }
   }
 
+  override fun wrapLilies(quantity: Int): List<Pair<Int, Int>> {
+    val result = mutableListOf<Pair<Int, Int>>()
+    val bundleSizes = listOf(9, 6, 3)
+    var remainingFlowers = quantity
+
+    bundleSizes.forEach { bundleSize ->
+      result.add(totalBundles(remainingFlowers, bundleSize))
+      remainingFlowers %= bundleSize
+    }
+
+    if (quantity < bundleSizes.min() || remainingFlowers > 0) throw Exception("Lilies can't be wrapped")
+
+    return result.filter { it.first > 0 }
+  }
+
   private fun totalBundles(flowers: Int, size: Int): Pair<Int, Int> {
     val quantity = flowers / size
     return Pair(quantity, size)
-  }
-
-  override fun wrapLilies(quantity: Int): List<Pair<Int, Int>> {
-    val bigBundle = 9
-    val mediumBundle = 6
-    val smallBundle = 3
-
-    val totalBigBundles = quantity / bigBundle
-    var remainingFlowers = quantity % bigBundle
-
-    val totalMediumBundles = remainingFlowers / mediumBundle
-    remainingFlowers %= mediumBundle
-
-    val totalSmallBundle = remainingFlowers / smallBundle
-    remainingFlowers %= smallBundle
-
-    if (quantity < smallBundle || remainingFlowers > 0) throw Exception("Lilies can't be wrapped")
-
-    return listOf(Pair(totalBigBundles, bigBundle), Pair(totalMediumBundles, mediumBundle), Pair(totalSmallBundle, smallBundle))
-      .filter { it.first > 0 }
   }
 
   override fun wrapTulips(quantity: Int): List<Pair<Int, Int>> {
